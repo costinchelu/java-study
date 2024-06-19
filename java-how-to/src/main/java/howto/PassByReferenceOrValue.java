f3c6b3916855d1bcd9683aa08e2153b93ba91348
costin.chelu <costin.chelu@johnlewis.co.uk>
2024-06-19 14:29:11 +0300

    .

--- java-how-to/src/main/java/howto/PassByReferenceOrValue.java
+++ java-how-to/src/main/java/howto/PassByReferenceOrValue.java
@@ -4,10 +4,10 @@ package howto;
 public class PassByReferenceOrValue {
 
     public static void main(String[] args) {
-        var roar1 = "roar";
-        var roar2 = new StringBuilder("roar");
-        int e = 1;
-        Integer ee = 1;
+        var roar1 = "roar";                     // IMMUTABLE
+        var roar2 = new StringBuilder("roar");  // MUTABLE
+        int e = 1;                              // VALUE
+        Integer ee = 1;                         // IMMUTABLE
         new Lion().roar(roar1, roar2, e, ee);
         System.out.println(roar1 + " " + roar2 + " " + e + " " + ee);
         /*
@@ -38,6 +38,35 @@ class Lion {
         roar2.append("!!!");
         primitive++;
         wrappper++;
+    }
+}
+
+class DragonWarriorReferenceChallenger {
+
+    public static void main(String... doYourBest) {
+        StringBuilder warriorProfession = new StringBuilder("Dragon ");     // mutable
+        String warriorWeapon = "Sword ";                                    // immutable
+
+        changeWarriorClass(warriorProfession, warriorWeapon);
+        /*
+        warriorProfession is indeed a mutable object. When it is passed to changeWarriorClass() a copy of the reference
+        to the actual object in the heap is created (specifically for that method call). But a copy to that reference means
+        that through that copy, we can still access the original object in the heap, as the original reference. So we can
+        indeed modify it. But when we assign null to that reference, we are only assigning null to the copy and with
+        that we are cutting the link of the copied reference to the actual object in the heap. Still, the original reference
+        from the main method is not affected by this assignment, because the assignment is done only for the local copy of
+        the reference.
+        * */
+        System.out.println("Warrior=" + warriorProfession + " &&  Weapon=" + warriorWeapon);
+    }
+
+    static void changeWarriorClass(StringBuilder warriorProfession, String weapon) {
+        warriorProfession.append("Knight");
+        weapon = "Dragon " + weapon;
+
+        weapon = null;
+        warriorProfession = null;
+        // assigning null to a reference variable only removes the reference, not the object itself
     }
 }
 
